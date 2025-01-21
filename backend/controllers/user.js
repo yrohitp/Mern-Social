2917b9c31c9b371a857fd4432801d383dcb14e70
yrohitp <yrohitp@gmail.com>
2025-01-22 03:03:53 +0530

    making more routes

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,12 +1,15 @@
+const { json } = require("express");
 const User = require("../models/User");
+const { TokenExpiredError } = require("jsonwebtoken");
 
 exports.register = async (req,res) => {
     try{
         const {name,email,password} = req.body;
 
-        let user = await User.findOe({email});
+        let user = await User.findOne({email});
         if(user){
-            return res.status(400)
+            return res
+            .status(400)
             .json({success : false, message : "User already exists"});
         }
 
@@ -14,14 +17,65 @@ exports.register = async (req,res) => {
             name,
             email,
             password,
-           //avtar {  public_id : "sampleid" , url : "sampleurl"},
+          // avtar {  public_id : "sampleid" , url : "sampleurl"},
         });
 
-        res.json
+        const token = await user.generateToken();
+        const option = {
+            expires : new Date(Date.now()+90*24*60*1000),
+                httpOnly : true,  
+        };
+        res.status(201).cookie("token" , token, option).json({
+            success:true,
+            user,
+            token
+        });
     }catch (error){
         res.status(500).json({
             success:false,
             message : error.message,
         })
+    }
+};
+
+exports.login = async (req,re) => {
+    try{
+        const { email ,password} = req.body;
+
+        const user = await user.findOne({email}).select("+password");
+
+        if(!user){
+            return res.status(400),json({
+                success : false,
+                message : "User does not exists"
+            })
+        }
+
+        const isMatch = await user.MatchPassword(password);
+
+        if(!isMatch){
+            return res.status(400).json({
+                success : false,
+                message : "Incorrect password",
+            });
+        }
+
+        const token = await user.generateToken();
+
+        const option = {
+            expires : new Date(Date.now()+90*24*60*1000),
+                httpOnly : true,  
+        }
+
+        res.status(200).cookie("token" , token, option).json({
+            success:true,
+            user,
+            token
+        });
+    }catch(error){
+        res.status(500).json({
+            success:false,
+            message:error.message,
+        });
     }
 };
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose") ;
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
     name : {
@@ -17,7 +19,7 @@ const userSchema = new mongoose.Schema({
         select : false,
     },
 
-   /* avtar : {
+   /*avtar : {
         public_id : String,
         url : string,
     },*/
@@ -37,5 +39,20 @@ const userSchema = new mongoose.Schema({
         ref:"User",
     },],
 });
+
+userSchema.pre("save",async function (next){
+    if(this.isModified("password")){
+        this.password = await bcrypt.hash(this.password,10);
+    }
+    next();
+});
+
+userSchema.methods.MatchPassword = async function (password) {
+    return await bcrypt.compare.hash(password,this.password);
+}
+
+userSchema.methods.generateToken = function() {
+    return jwt.sign({_id : this._id},process.env.JWT_SECRET);
+};
 
 module.exports = mongoose.model("User" , userSchema);
